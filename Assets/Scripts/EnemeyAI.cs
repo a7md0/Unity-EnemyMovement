b5c691e791e9536addb18baa9d77402397c892ee
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-04-26 12:01:38 +0300

    Implement nav

---
+++ Assets/Scripts/EnemeyAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemeyAI : MonoBehaviour {
+    private NavMeshAgent agent;
+    public GameObject target;
+
+    // Use this for initialization
+    void Start() {
+        this.agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        this.agent.destination = this.target.transform.position;
+    }
+}
